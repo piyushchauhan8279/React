1b5f642ff1a06a00d0bd78b1d9844004e0feb7a8
Piyush Chauhan <piyushchauhan70900@gmail.com>
2025-07-24 15:14:11 +0530

    add

--- To-do-List/src/App.jsx
+++ To-do-List/src/App.jsx
@@ -3,38 +3,19 @@ import AddTodo from "./components/AddTodo";
 import TodoItems from "./components/TodoItems";
 import WelcomeMsg from "./components/WelcomeMsg";
 
-import { useState } from "react";
-import { TodoItemsContext } from "./store/todo-items-context";
-
+import TodoItemsContextProvider from "./store/todo-items-context";
 function App() {
-  let [taskItem, settaskItem] = useState([]);
-
-  let handleTaskItem = (dueTask, dueDate) => {
-    if (dueTask && dueDate) {
-      settaskItem([...taskItem, { task: dueTask, dueDate }]);
-    }
-  };
-
-  let handleDeleteBtn = (deletedItem) => {
-    settaskItem(taskItem.filter((item) => item.task != deletedItem));
-  };
-
   return (
     <>
-      <TodoItemsContext.Provider
-        value={{
-          taskItem,
-          newTaskItem: handleTaskItem,
-          onClickBtn: handleDeleteBtn,
-        }}
-      >
+    <TodoItemsContextProvider>
         <center>
           <AppName />
           <AddTodo />
           <WelcomeMsg />
           <TodoItems />
         </center>
-      </TodoItemsContext.Provider>
+    </TodoItemsContextProvider>
+      
     </>
   );
 }
--- To-do-List/src/store/todo-items-context.jsx
+++ To-do-List/src/store/todo-items-context.jsx
@@ -1,3 +1,62 @@
 import { createContext } from "react";
+import { useReducer } from "react";
 
-export let TodoItemsContext=createContext({taskItem:[],newTaskItem:()=>{} ,onClickBtn:()=>{}});
+export let TodoItemsContext = createContext({
+  taskItem: [],
+  newTaskItem: () => {},
+  onClickBtn: () => {},
+});
+
+let TodoItemsContextProvider = ({ children }) => {
+  // reducer method
+  let reducer = (taskItem, action) => {
+    let newTask = [];
+    if (action.type === "NEW_TODO") {
+      newTask = [
+        ...taskItem,
+        { task: action.payload.dueTask, dueDate: action.payload.dueDate },
+      ];
+    } else if (action.type === "DELETE_TODO") {
+      newTask = taskItem.filter(
+        (item) => item.task != action.payload.deletedItem
+      );
+    }
+    return newTask;
+  };
+
+  const [taskItem, dispatch] = useReducer(reducer, []);
+
+  let handleTaskItem = (dueTask, dueDate) => {
+    let action = {
+      type: "NEW_TODO",
+      payload: { dueTask, dueDate },
+    };
+
+    dispatch(action);
+  };
+
+  let handleDeleteBtn = (deletedItem) => {
+    let action = {
+      type: "DELETE_TODO",
+      payload: {
+        deletedItem,
+      },
+    };
+    dispatch(action);
+  };
+
+  return <>
+  <TodoItemsContext.Provider
+        value={{
+          taskItem,
+          newTaskItem: handleTaskItem,
+          onClickBtn: handleDeleteBtn,
+        }}
+       
+      >
+        {children}
+      </TodoItemsContext.Provider>
+  </>;
+};
+
+export default TodoItemsContextProvider;
