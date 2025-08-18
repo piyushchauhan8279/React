ca39695836a3d3e507f44aa31179eb16e6eef6df
Piyush Chauhan <piyushchauhan70900@gmail.com>
2025-08-18 07:49:04 +0530

    add

---
+++ State-Obj/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Ludo from './Ludo'
+function App() {
+
+
+  return (
+    <>
+      <Ludo></Ludo>
+    </>
+  )
+}
+
+export default App
---
+++ State-Obj/src/Ludo.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+export default function Ludo() {
+  let [moves, setMoves] = useState({ green: 0, red: 0, yellow: 0, blue: 0 });
+  let [arr,setArr]=useState(["no moves"]);
+  let handleGreen = () => {
+    // setMoves(() => {
+    //   return { ...moves, green: moves.green + 1 };
+    // });
+
+    setArr((prevMove)=>{
+      console.log(prevMove);
+      return [...prevMove,' GREEN MOVES  ']
+
+    })
+  };
+  let handleBlue = () => {
+    setMoves(() => {
+      return { ...moves, blue: moves.blue + 1 };
+    });
+  };
+  let handleRed = () => {
+    setMoves(() => {
+      return { ...moves, red: moves.red + 1 };
+    });
+  };
+  let handleYellow = () => {
+    setMoves(() => {
+      return { ...moves, yellow: moves.yellow + 1 };
+    });
+  };
+
+  return (
+    <div>
+      <p>{arr}</p>
+      <p>Green count:{moves.green}</p>
+      <button style={{ backgroundColor: "green" }} onClick={handleGreen}>
+        +1
+      </button>
+      <p>red count:{moves.red}</p>
+      <button style={{ backgroundColor: "red" }} onClick={handleRed}>
+        +1
+      </button>
+      <p>yellow count:{moves.yellow}</p>
+      <button
+        style={{ backgroundColor: "yellow", color: "black" }}
+        onClick={handleYellow}
+      >
+        +1
+      </button>
+      <p>blue count:{moves.blue}</p>
+      <button style={{ backgroundColor: "blue" }} onClick={handleBlue}>
+        +1
+      </button>
+    </div>
+  );
+}
